be256dab724687e8c3d7d6f52c1b263e2de6d439
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-04-04 19:32:20 +0200

    always_run: this hook will now run even if there are no matching files

--- pre_commit_hooks/bandit_analyzer.py
+++ pre_commit_hooks/bandit_analyzer.py
@@ -1,9 +1,12 @@
 from __future__ import print_function
 import sys
+import bandit
 from bandit.cli.main import main as bandit_cli_main
 
 
 def main():
+    print(f"Bandit version: {bandit.__version__}")
+    print("Calling", " ".join(sys.argv))
     try:
         bandit_cli_main()
         return 0
